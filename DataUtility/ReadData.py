3923520d6f2b90f1e474c59192c9e28ca53c8c7d
Tyler Evans <ty.evans001@gmail.com>
2019-01-17 00:11:28 -0600

    Add preliminary heuristics and data utils.

---
+++ DataUtility/ReadData.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def read_board(path):
+    lines = open(path).readlines()
+    lines = map(lambda x: x.strip(), lines)
+
+    data = np.array([list(x) for x in lines if x][2:-1])
+    n = data.shape[0]
+
+    return data, n
---
+++ Heuristic/Heuristic.py
@@ -0,0 +1,11 @@
+import random
+
+
+def random_heuristic(variables, csp):
+    return random.sample(variables, 1)[0]
+
+
+def most_constrained_heuristic(variables, csp):
+    counts = csp.get_unasigned_domain_num_consistent_counts()
+    variables = sorted(counts, key=counts.get) # sorted low to high by count
+    return variables[0]
