cdb59404339e784500a29ad548e587a8ab73d167
KeatonMacLeod <macleodkeaton@gmail.com>
2019-02-04 23:10:44 -0600

    Added additional comments for constraint heuristics

--- Heuristic/Heuristic.py
+++ Heuristic/Heuristic.py
@@ -2,11 +2,14 @@ import random
 import numpy as np
 
 
+# Chooses a randomly currently unassigned node
 def random_heuristic(csp):
     unassigned_variable_index = random.randint(0, len(csp.unassigned_variables) - 1)
     return _convert_index(csp, unassigned_variable_index)
 
 
+# Chooses an unassigned node with the smallest number of domain options in the CSP ->
+# thus making it the most_constrained choice
 def most_constrained_node_heuristic(csp, domains_are_consistent):
     counts = _get_counts(csp, domains_are_consistent)
     min_indices = np.where(counts == np.min(counts))[0]
@@ -14,6 +17,10 @@ def most_constrained_node_heuristic(csp, domains_are_consistent):
     return _convert_index(csp, unassigned_variable_index)
 
 
+# Chooses an unassigned node with the greatest number of domain options in the CSP -> what this corresponds to within
+# the row + column based representation is that, upon choosing the row or column variable with the greatest number
+# of domain options, instantiating THIS variable with a value will cause the most amount of domain reductions in every
+# other node in the graph, thus making it the most_constraining node
 def most_constraining_node_heuristic(csp, domains_are_consistent):
     counts = _get_counts(csp, domains_are_consistent)
     max_indices = np.where(counts == np.max(counts))[0]
